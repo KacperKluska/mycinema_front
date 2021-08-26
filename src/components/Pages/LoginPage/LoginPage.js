426fd4b3db6aae0f37d33d3c8e38397b37af370f
KacperKluska <kacperkluska130@gmail.com>
2021-08-27 00:47:29 +0200

    bug fixed

--- src/components/Pages/LoginPage/LoginPage.js
+++ src/components/Pages/LoginPage/LoginPage.js
@@ -1,7 +1,8 @@
 import "./style.scss";
 import Login from "../../Login/Login";
+import Register from "../../Register/Register";
 
-export default function Register() {
+export default function LoginPage() {
   return (
     <div className="emptyContent">
       <article className="content">
