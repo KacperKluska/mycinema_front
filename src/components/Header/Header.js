94baf3f2cc799748d7bdb2de23d1e8c132406f45
KacperKluska <kacperkluska130@gmail.com>
2021-08-26 13:04:15 +0200

    added account button to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,6 +24,7 @@ export default function Header() {
         )}
         {userLogged && (
           <ul>
+            <li onClick={LogOut}>Account</li>
             <li onClick={LogOut}>Log out</li>
           </ul>
         )}
