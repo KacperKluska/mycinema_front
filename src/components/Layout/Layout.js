e25c37f00e18fd2022c1bf1ca2afaf12953eb7b6
KacperKluska <kacperkluska130@gmail.com>
2021-08-26 22:33:31 +0200

    added basic routing to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { UserStatusContextProvider } from "../Context/UserStatusContext";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import Account from "../Pages/Account/Account";
 import MainPage from "../Pages/MainPage/MainPage";
 
 export default function Layout() {
@@ -10,7 +11,14 @@ export default function Layout() {
       <UserStatusContextProvider>
         <Header />
         <main id="emptyPage">
-          <MainPage />
+          <Switch>
+            <Route exact path="/">
+              <MainPage />
+            </Route>
+            <Route exact path="/account">
+              <Account />
+            </Route>
+          </Switch>
         </main>
         <Footer />
       </UserStatusContextProvider>
