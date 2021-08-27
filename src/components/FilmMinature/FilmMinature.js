aaf5af6858d7b60c8e0eb199cf4b40671a2dce59
KacperKluska <kacperkluska130@gmail.com>
2021-08-28 01:35:19 +0200

    some changes to film minature

--- src/components/FilmMinature/FilmMinature.js
+++ src/components/FilmMinature/FilmMinature.js
@@ -8,10 +8,26 @@ export default function FilmMinature({ film }) {
       <ImageMinature url={film.img} />
       <section className="filmDescription">
         <FilmHeader>{film.title}</FilmHeader>
-        <label>Release: {film.release}</label>
-        <label>Length: {film.length}</label>
-        <label>PEGI: {film.pegi}</label>
-        <label>Critics rate: {film.userRate}/10</label>
+        <div className="descriptionLine">
+          <label className="descriptionLabel">Release: </label>
+          {film.release}
+        </div>
+        <div className="descriptionLine">
+          <label className="descriptionLabel">Length: </label>
+          {film.length}
+        </div>
+        <div className="descriptionLine">
+          <label className="descriptionLabel">PEGI: </label>
+          {film.pegi}
+        </div>
+        <div className="descriptionLine">
+          <label className="descriptionLabel">Critics rate: </label>
+          {film.userRate}/10
+        </div>
+        <div className="descriptionLine">
+          <label className="descriptionLabel">Description: </label>
+          {film.description}
+        </div>
       </section>
     </article>
   );
