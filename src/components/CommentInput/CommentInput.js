79bce37ca4ce7841d21da0eab14516d8559628bb
KacperKluska <kacperkluska130@gmail.com>
2021-09-05 00:48:22 +0200

    blocked adding comment if not logged

--- src/components/CommentInput/CommentInput.js
+++ src/components/CommentInput/CommentInput.js
@@ -1,11 +1,15 @@
 import Button from "../Button/Button";
 import "./style.scss";
+import { useContext } from "react";
+import { UserStatusContext } from "../Context/UserStatusContext";
 
 export default function CommentInput({
   handleNumber,
   handleText,
   handleButtonCLick,
 }) {
+  const [userLogged] = useContext(UserStatusContext);
+
   return (
     <form className="commentForm" onSubmit={handleButtonCLick}>
       <div className="commentUpperRow">
@@ -18,7 +22,13 @@ export default function CommentInput({
           placeholder="Your rate..."
           onChange={(e) => handleNumber(e.target.value)}
           required
+          disabled={userLogged ? false : true}
         ></input>
+        {!userLogged && (
+          <div className="needToBeLogged">
+            You need to be logged in to post a comment
+          </div>
+        )}
         <Button color="blue" type="submit">
           Comment!
         </Button>
@@ -29,6 +39,7 @@ export default function CommentInput({
         placeholder="Write a comment..."
         onChange={(e) => handleText(e.target.value)}
         required
+        disabled={userLogged ? false : true}
       ></textarea>
     </form>
   );
